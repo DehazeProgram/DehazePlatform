0360e1682eb7668dd4d3f95f45c5a257ad623350
huangrui <huangrui1991@outlook.com>
2014-12-10 16:16:36 +0800

    git add slic file

--- ColorFactory/ColorFactory.pro
+++ ColorFactory/ColorFactory.pro
@@ -11,11 +11,13 @@ DEFINES += COLORFACTORY_LIBRARY
 HEADERS += \
     colorcorrect.h\
     filter.h\
-    colorfactory_global.h
+    colorfactory_global.h\
+    slic.h
 
 SOURCES += \
     colorcorrect.cpp\
-    filter.cpp
+    filter.cpp\
+    slic.cpp
 
 win32:CONFIG(release, debug|release):LIBS += -LC:/OSGeo4W64/lib/ -lopencv_core248 -lopencv_highgui248 -lopencv_gpu248 -lopencv_imgproc248 -lopencv_features2d248 -lopencv_photo248
 else:win32:CONFIG(debug, debug|release):LIBS += -L/C:/OSGeo4W64/lib/ -lopencv_core248d -lopencv_highgui248d
@@ -23,3 +25,10 @@ else:unix: LIBS += -LC:/OSGeo4W64/lib/ -lopencv_core -lopencv_highgui
 
 INCLUDEPATH += C:/OSGeo4W64/include
 DEPENDPATH += C:/OSGeo4W64/include
+
+win32:CONFIG(release, debug|release): LIBS += -L$$PWD/../../../../OSGeo4W64/lib/ -lgdal_i
+else:win32:CONFIG(debug, debug|release): LIBS += -L$$PWD/../../../../OSGeo4W64/lib/ -lgdal_id
+else:unix: LIBS += -L$$PWD/../../../../OSGeo4W64/lib/ -lgdal_i
+
+INCLUDEPATH += $$PWD/../../../../OSGeo4W64/include
+DEPENDPATH += $$PWD/../../../../OSGeo4W64/include
---
+++ ColorFactory/slic.h
@@ -0,0 +1,46 @@
+#ifndef SLIC_H
+#define SLIC_H
+
+#include <iostream>
+#include <vector>
+#include <gdal_priv.h>
+
+typedef std::vector<double> FeatureVector;
+
+class Slic
+{
+public:
+    Slic(GDALDataset* &poSrcDS,GDALDataset* &poDstDS,int regionSize,double regularizer);
+    bool StartSegmentation();
+
+private:
+    //Params
+    GDALDataset*    _poSrcDS;  //Any Data Type
+    GDALDataset*    _poDstDS;  //GDT_Int32
+    int             _regionSize;
+    double          _regularizer;
+
+    //Temp var
+    int             _width;
+    int             _height;
+    int             _bandCount;
+
+    int             _M;         //SLIC starts by dividing the image domain into a regular grid with M×N tiles
+    int             _N;    
+    GDALDataType    _dataType;  //GDALDatatype
+    int             _dataSize;  //Data size per pixel
+
+    std::vector< FeatureVector  >       _centerVector;
+    std::vector< std::vector<double> >  _normalizationParam;
+
+    //Temp Funcs
+    bool    _InitData();
+    void    _InitCenterFeature (int i, int j, FeatureVector &featureVec);
+    void    _GenerateSuperpixels();
+    int     _GetNearestCenter(const std::vector<int> &candidateCenterID, const FeatureVector &featureVec);
+    double  _ComputeDistance(const FeatureVector &vec1,const FeatureVector &vec2);
+    void    _ComputeNewCenterVector();
+    void    _ElininateSmallSuperpixel();
+};
+
+#endif //SLIC_H
